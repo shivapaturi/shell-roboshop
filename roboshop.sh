8f4ad037be91a16d769fb5ce1b4fc3777b3ca0f9
shiva paturi <shivapaturi01@gmail.com>
2025-05-25 20:55:34 +0530

    loop

--- roboshop.sh
+++ roboshop.sh
@@ -6,14 +6,16 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_id="Z0002010ZLBCRXD0A652"
 DOMAIN_NAME="daws84ss.site"
 
-for INSTANCES in $@
-do 
-   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-010eb25d82d15b248 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query '.Instances[0].InstanceID' --output text)
-   if [ $instance != "frontend" ]
-   then
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+       
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress]" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+       
     fi
     echo "$instance IP address: $IP"
-done
