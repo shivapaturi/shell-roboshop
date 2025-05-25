d34005df94771cc609253e99b7e3f1a5af2005aa
shiva paturi <shivapaturi01@gmail.com>
2025-05-25 23:52:59 +0530

    hello world started

---
+++ catalogue.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#check the usr has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+dnf module list nodejs &>>$LOG_FILE
+VALIDATE $? "module listing nodejs" 
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "module disabling nodejs" 
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "module enabling nodejs:20" 
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs" 
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "creating roboshop system user" 
+
+mkdir /app 
+VALIDATE $? "creating app directory" 
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading the catalogue" 
+
+cd /app 
+VALIDATE $? "Moving to the catalogue" 
+
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping the catalogue" 
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing the Dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "Copying Catalogue service" 
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Reloading Catalogue service" 
+
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enabling Catalogue service" 
+
+systemctl start catalogue
+VALIDATE $? "Starting Catalogue service" 
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+VALIDATE $? "Copying mongodb repo" 
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb server" 
+
+mongosh --host mongodb84ss.site </app/db/master-data.js &>>$LOG_FILE
+
