bf6f4b9417b291f67dcd85f9ed77376647ddf25b
shiva paturi <shivapaturi01@gmail.com>
2025-05-26 00:46:22 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-#check the usr has root priveleges or not
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
@@ -32,59 +32,50 @@ VALIDATE(){
         exit 1
     fi
 }
-dnf module list nodejs &>>$LOG_FILE
-VALIDATE $? "module listing nodejs" 
 
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "module disabling nodejs" 
+VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "module enabling nodejs:20" 
+VALIDATE $? "Enabling nodejs:20"
 
 dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs" 
+VALIDATE $? "Installing nodejs:20"
 
 id roboshop
 if [ $? -ne 0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "creating roboshop system user" 
+    VALIDATE $? "Creating roboshop system user"
 else
-    echo -e "System is already created roboshop user..$Y SKPPING $N"
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app 
-VALIDATE $? "creating app directory" 
+VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "downloading the catalogue" 
+VALIDATE $? "Downloading Catalogue"
 
+rm -rf /app/*
 cd /app 
-VALIDATE $? "Moving to the catalogue" 
-
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping the catalogue" 
+VALIDATE $? "unzipping catalogue"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing the Dependencies"
+VALIDATE $? "Installing Dependencies"
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
-VALIDATE $? "Copying Catalogue service" 
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
 
 systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "Reloading Catalogue service" 
-
-systemctl enable catalogue &>>$LOG_FILE
-VALIDATE $? "Enabling Catalogue service" 
-
+systemctl enable catalogue  &>>$LOG_FILE
 systemctl start catalogue
-VALIDATE $? "Starting Catalogue service" 
+VALIDATE $? "Starting Catalogue"
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
-VALIDATE $? "Copying mongodb repo" 
-
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-VALIDATE $? "Installing mongodb server" 
+VALIDATE $? "Installing MongoDB Client"
 
 STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
@@ -94,3 +85,4 @@ then
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
+
