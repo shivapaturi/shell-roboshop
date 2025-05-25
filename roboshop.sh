3ed9f8f72aacf4ea67336976271022dea7c06722
shiva paturi <shivapaturi01@gmail.com>
2025-05-25 20:57:12 +0530

    loop

--- roboshop.sh
+++ roboshop.sh
@@ -16,6 +16,5 @@ do
        
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-       
     fi
     echo "$instance IP address: $IP"
