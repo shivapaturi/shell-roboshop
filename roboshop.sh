ee93a8c9d1d8c13bb7fe1a10e955487c98469c4d
shiva paturi <shivapaturi01@gmail.com>
2025-05-25 20:52:21 +0530

    loop

--- roboshop.sh
+++ roboshop.sh
@@ -6,14 +6,14 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_id="Z0002010ZLBCRXD0A652"
 DOMAIN_NAME="daws84ss.site"
 
-for INSTANCES in "${INSTANCES[@]}"
+for INSTANCES in $@
 do 
    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-010eb25d82d15b248 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test$instance}]" --query '.Instances[0].InstanceID' --output text)
    if [ $instance != "frontend" ]
    then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].[InstanceId, PrivateIpAddress]" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
             else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].[InstanceId, PublicIpAddress]" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress]" --output text)
     fi
     echo "$instance IP address: $IP"
 done
